5698c215f3fd583aabd64d1b8cbc14d41e3e6ccd
Mohanesh <geekymohanesh@gmail.com>
2017-09-01 00:39:02 +0530

    Latest changes - passing values between components

--- day-planner/src/App.js
+++ day-planner/src/App.js
@@ -22,10 +22,14 @@
 
 import React from 'react';
 import HelloWorld from './HelloWorld';
-
+import HelloWorldList from './HelloWorldList';
 import './App.css';
 
 const App = () => {
-  return (<div className = "App"> <HelloWorld/><HelloWorld/> </div>);
+  return (
+    <div className="App">
+      <HelloWorldList />
+    </div>
+  );
 }
 export default App;
--- day-planner/src/HelloWorld.js
+++ day-planner/src/HelloWorld.js
@@ -1,7 +1,31 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './HelloWorld.css';
 
-const HelloWorld = () => {
-    return (<div className = "HelloWorld"> Hello World! </div>); 
-};
+class HelloWorld extends Component{
+    constructor(props){
+        super(props);
+        this.state = {greeting: 'Hello'};
+        this.frenchify = this.frenchify.bind(this);
+        this.removeGreeting = this.removeGreeting.bind(this);
+    }
+    frenchify(){
+        this.setState({greeting: 'Bonjour'});
+    }
+    removeGreeting() {
+        this.props.removeGreeting(this.props.name);
+      }
+    render(){
+        return (
+            <div className = "HelloWorld"> 
+                {this.state.greeting} 
+                {this.props.name}! 
+                <br/>
+                <button onClick = {this.frenchify}>Frenchify!</button>
+                <button onClick={this.removeGreeting}>Remove Me!</button>
+            </div>
+
+        ); 
+    }
+}
+
 export default HelloWorld;
